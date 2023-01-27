be328003b7be5f115776edb30c5cab4523168896
Julio Nunes <julio.nt753@gmail.com>
2023-01-26 21:29:32 -0300

    adicionado js na pagina do produto

--- script.js
+++ script.js
@@ -31,3 +31,15 @@ function setQuantidade(input) {
 	total = document.getElementById('valorTotal').textContent;
 	console.log(total);
 }
+
+function changeImage(thumbnail) {
+	document.getElementById('display').src = thumbnail;
+}
+
+function clickedImage(id) {
+	document.getElementById('img-mini-1').style.opacity = 1;
+	document.getElementById('img-mini-2').style.opacity = 1;
+	document.getElementById('img-mini-3').style.opacity = 1;
+
+	document.getElementById(id).style.opacity = 0.5;
+}
